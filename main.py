69c60f05b24472ad0ab0afdabbe76b26e19e6644
MattFrench019 <57752799+mattfrench019@users.noreply.github.com>
2019-11-28 10:27:27 +0000

    Added Portals

--- main.py
+++ main.py
@@ -3,5 +3,20 @@ import flask
 app = flask.Flask(__name__)
 
 
+@app.route('/waiter', methods=['GET'])
+def waiter_portal():
+	flask.abort(501, '501 Not Implemented')
+
+
+@app.route('/chef', methods=['GET'])
+def chef_portal():
+	flask.abort(501, '501 Not Implemented')
+
+
+@app.route('/manager', methods=['GET'])
+def manager_portal():
+	flask.abort(501, '501 Not Implemented')
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
