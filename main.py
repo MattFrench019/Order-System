24f061dd6b9bbd26903dbad676f99a64a7617f6a
MattFrench019 <57752799+mattfrench019@users.noreply.github.com>
2019-11-28 11:00:53 +0000

    Backend Handles

--- main.py
+++ main.py
@@ -3,6 +3,29 @@ import flask
 app = flask.Flask(__name__)
 
 
+### --------Backend Handles------- ###
+@app.route('/orders/get/all', methods=['GET'])
+def get_orders():
+	flask.abort(501, '501 Not Implemented')
+
+
+@app.route('/orders/new', methods=['POST'])
+def add_order():
+	flask.abort(501, '501 Not Implemented')
+
+
+@app.route('/orders/get/<int:order_id>', methods=['GET'])
+def get_order(order_id):
+	flask.abort(501, '501 Not Implemented')
+
+
+@app.route('/orders/edit/<int:order_id>', methods=['POST'])
+def edit_order(order_id):
+	flask.abort(501, '501 Not Implemented')
+
+
+
+### -------Frontend Portals------- ###
 @app.route('/waiter', methods=['GET'])
 def waiter_portal():
 	flask.abort(501, '501 Not Implemented')
